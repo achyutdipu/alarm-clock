9fc9e88057f3b69d05b85414bb5a4c45e741daf3
Achyut <achyutdipu@gmail.com>
2020-10-31 19:30:44 -0400

    Sacing again

--- alarm-clock-javascript-file.js
+++ alarm-clock-javascript-file.js
@@ -8,4 +8,14 @@ var startstop = document.getElementById('startstop');
 var currentTime;
 var alarmTime;
 var activeColumn = false;
-var sound = new Audio('alarm.mp3')
+var sound = new Audio('alarm.mp3');
+sound.loop = true;
+
+function showTime() {
+    while (true) {
+    var now = new Date();
+    currentTime = now.toLocaleTimeString();
+    timer.textContent = currentTime;
+    }
+}
+showTime();
