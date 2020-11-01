47e5a3c46c584f9836a7f5ab4e9210ee7880b571
Achyut <achyutdipu@gmail.com>
2020-11-01 16:28:00 -0500

    new saving

--- alarm-clock-javascript-file.js
+++ alarm-clock-javascript-file.js
@@ -6,15 +6,69 @@ var ampm = document.getElementById('ampm');
 var startstop = document.getElementById('startstop');
 
 var currentTime;
-var alarmTime;
-var activeColumn = false;
+var alarmElement;
+var activeAlarm = false;
 var sound = new Audio("alarm.mp3");
 sound.loop = true;
 
 function showTime() {
     var now = new Date();
     currentTime = now.toLocaleTimeString();
+        
+    if(currentTime === alarmElement) {
+        sound.play();
+    }
     timer.textContent = currentTime;
     setTimeout(showTime, 1000);
 }
 showTime();
+
+function addMinSec(id) {
+    var select = id;
+    var min = 59;
+
+    for (i = 0; i <=min; i++) {
+        select.options[select.options.length] = new Option(i <10 ? '0' + i: i);
+        
+    }
+}
+
+addMinSec(seconds);
+addMinSec(minutes);
+
+function addHours(id) {
+    var select = id;
+    var min = 12;
+
+    for (i = 0; i <=min; i++) {
+        select.options[select.options.length] = new Option(i <10 ? i: i);
+        
+    }
+}
+
+addHours(hours);
+
+startstop.onclick = function() {
+   
+    if(activeAlarm === false) {
+        hours.disabled = true;
+        minutes.disabled = true;
+        seconds.disabled = true;
+        ampm.disabled = true;
+
+        alarmElement = hours.value + ':' + minutes.value + ':' + seconds.value + ' ' + ampm.value;
+        this.textContent = "Clear Alarm";
+        activeAlarm = true;
+
+    }
+    else{
+        hours.disabled = false;
+        minutes.disabled = false;
+        seconds.disabled = false;
+        ampm.disabled = false;
+
+        alarmElement = hours.value + ':' + minutes.value + ':' + seconds.value + ' ' + ampm.value;
+        this.textContent = "Set Alarm";
+        activeAlarm = false;
+    }
+}
